006fe9b47ba09ec5ccb2a505e620d00512c9bd82
daoudacherif <daoudacherif4321@gmail.com>
2025-07-31 16:24:52 +0000

    Enable error reporting for debugging in supplier-payments.php

--- admin/supplier-payments.php
+++ admin/supplier-payments.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(0);
+
 include('includes/dbconnection.php');
 
 if (strlen($_SESSION['imsaid'] == 0)) {
